7ac770c877c81bd0b0c94b3d8c6e8c835f0b7e1b
Hughsin23 <hughsy_23@hotmail.com>
2024-03-26 22:16:19 +1100

    fixed bug by putting search hook outside app

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,17 @@ import { useState, useEffect } from "react";
 import "./App.css";
 import ErrorBoundary from "./components/ErrorBoundary";
 
+
+
+
+
+
+
 function App() {
   // take the search function and animals state from the useAnimalSearch custom hook
   const { search, animals } = useAnimalSearch();
 
-  // A custom hook to have the animals able to be searched with a fetch to our own server, then updates the state to the search results. The last query is stored in local storage for user QOL
-  const useAnimalSearch = () => {
-    const [animals, setAnimals] = useState([]);
 
-    // grabs last search from local storage
-    useEffect(() => {
-      const lastUserQuery = localStorage.getItem("lastUserQuery");
-      search(lastUserQuery);
-    }, []);
-
-    // search function to grab from our server
-    const search = async (q) => {
-      const response = await fetch(`http://localhost:8080?${q}`);
-      const data = await response.json();
-      setAnimals(data);
-
-      localStorage.setItem("lastUserQuery", q);
-    };
-
-    // return the search function for use in the GUI, and the animal state
-    return { search, animals };
-  };
 
   return (
     <main>
@@ -36,7 +21,7 @@ function App() {
       <input
         type="text"
         placeholder="Search"
-        onChange={(e) => console.log(e.target.value)}
+        onChange={(e) => search(e.target.value)}
       ></input>
       <ErrorBoundary fallback={<p>Something went wrong 😭</p>}>
         <ul>
@@ -47,11 +32,37 @@ function App() {
               {animal.age}
             </li>
           ))}
+          {animals.length === 0 && "No animals found"}
         </ul>
-        {animals.length === 0 && "No animals found"}
       </ErrorBoundary>
     </main>
   );
+
 }
+
+
+// A custom hook to have the animals able to be searched with a fetch to our own server, then updates the state to the search results. The last query is stored in local storage for user QOL
+const useAnimalSearch = () => {
+  const [animals, setAnimals] = useState([]);
+
+  // grabs last search from local storage
+  useEffect(() => {
+    const lastUserQuery = localStorage.getItem("lastUserQuery");
+    search(lastUserQuery);
+  }, []);
+
+  // search function to grab from our server
+  const search = async (q) => {
+    const response = await fetch(`http://localhost:8080?${q}`);
+    const data = await response.json();
+    setAnimals(data);
+
+    localStorage.setItem("lastUserQuery", q);
+  };
+
+  // return the search function for use in the GUI, and the animal state
+  return { search, animals };
+};
+
 
 export default App;
