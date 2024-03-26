cb11faa12b6ee303c6a02a0587b73eb91ca01f37
Vivian Cao <viviancao58@gmail.com>
2024-03-26 22:26:49 +1100

    reorder logical
    Co-authored-by: Hugh <Hughsin23@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,34 @@ import { useState, useEffect } from "react";
 import "./App.css";
 import ErrorBoundary from "./components/ErrorBoundary";
 
+// A custom hook to have the animals able to be searched with a fetch to our own server, then updates the state to the search results. The last query is stored in local storage for user QOL
+const useAnimalSearch = () => {
+  const [animals, setAnimals] = useState([]);
 
+  // grabs last search from local storage
+  useEffect(() => {
+    const lastUserQuery = localStorage.getItem("lastUserQuery");
+    search(lastUserQuery);
+  }, []);
 
+  // search function to grab from our server
+  const search = async (q) => {
+    const response = await fetch(
+      `http://localhost:8080?` + new URLSearchParams({ q })
+    );
+    const data = await response.json();
+    setAnimals(data);
 
+    localStorage.setItem("lastUserQuery", q);
+  };
 
-
+  // return the search function for use in the GUI, and the animal state
+  return { search, animals };
+};
 
 function App() {
   // take the search function and animals state from the useAnimalSearch custom hook
   const { search, animals } = useAnimalSearch();
-
-
 
   return (
     <main>
@@ -37,32 +54,6 @@ function App() {
       </ErrorBoundary>
     </main>
   );
-
 }
-
-
-// A custom hook to have the animals able to be searched with a fetch to our own server, then updates the state to the search results. The last query is stored in local storage for user QOL
-const useAnimalSearch = () => {
-  const [animals, setAnimals] = useState([]);
-
-  // grabs last search from local storage
-  useEffect(() => {
-    const lastUserQuery = localStorage.getItem("lastUserQuery");
-    search(lastUserQuery);
-  }, []);
-
-  // search function to grab from our server
-  const search = async (q) => {
-    const response = await fetch(`http://localhost:8080?${q}`);
-    const data = await response.json();
-    setAnimals(data);
-
-    localStorage.setItem("lastUserQuery", q);
-  };
-
-  // return the search function for use in the GUI, and the animal state
-  return { search, animals };
-};
-
 
 export default App;
