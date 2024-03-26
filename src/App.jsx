7d4d0554f3fbd82c0e6dccc7214d5c70156d2a2f
Vivian Cao <viviancao58@gmail.com>
2024-03-26 21:40:14 +1100

    add front-end title, description and input search bar
    Co-authored-by: Hugh <Hughsin23@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main>
+      <h1>Hugh and Vivian's Animal Farm</h1>
+      <p>Search and see what animals we've got going on</p>
+      <input
+        type="text"
+        placeholder="Search"
+        onChange={(e) => console.log(e.target.value)}
+      ></input>
+    </main>
+  );
 }
 
-export default App
+export default App;
