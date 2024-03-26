255bb10322ab0dae69c1d6325d53ad18179c43b4
Vivian Cao <viviancao58@gmail.com>
2024-03-26 21:19:24 +1100

    add endpoint for animal search & generate animals array
    Co-authored-by: Hugh <Hughsin23@users.noreply.github.com>

--- server/index.js
+++ server/index.js
@@ -1,19 +1,36 @@
-import express from "express"
-import cors from 'cors'
+import express from "express";
+import cors from "cors";
 // minimalist generator of random strings, numbers etc
-import Chance from 'chance'
+import Chance from "chance";
 
+//initialise the express app
+const app = express();
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+// middleware for cors and express to parse json
+app.use(cors());
+app.use(express.json());
 
-const chance = new Chance()
+const chance = new Chance();
 
-app.get('jokes', (req, res) => {
-  const q = req.query.q?.toLocaleLowerCase() || ''
-  const results = jokes.filter(joke => joke.type.toLowerCase().includes(q))
-})
+const animals = [...Array(300).keys()].map((id) => {
+  return {
+    id,
+    type: chance.animal(),
+    age: chance.age(),
+    name: chance.name(),
+  };
+});
 
+//endpoint to search for animals
+app.get("animals", (req, res) => {
+  const q = req.query.q?.toLocaleLowerCase() || "";
+  const results = animals.filter((animal) =>
+    animal.type.toLocaleLowerCase().includes(q)
+  );
 
-app.listen(8080, () => { console.log('hello 8080'); })
+  res.send(results);
+});
+
+app.listen(8080, () => {
+  console.log("hello 8080");
+});
